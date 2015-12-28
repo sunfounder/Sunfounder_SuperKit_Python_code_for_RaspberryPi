f3e589863d16479842e584945e176f8d13a9e552
Cavon <lijiarong@sunfounder.com>
2015-12-28 12:21:10 +0800

    re-arrange the code

--- 04_pwmLed.py
+++ 04_pwmLed.py
@@ -4,13 +4,16 @@ import time
 
 LedPin = 12
 
-GPIO.setmode(GPIO.BOARD)       # Numbers GPIOs by physical location
-GPIO.setup(LedPin, GPIO.OUT)   # Set LedPin's mode is output
-GPIO.output(LedPin, GPIO.LOW)  # Set LedPin to low(0V)
+def setup():
+	global p
+	GPIO.setmode(GPIO.BOARD)       # Numbers GPIOs by physical location
+	GPIO.setup(LedPin, GPIO.OUT)   # Set LedPin's mode is output
+	GPIO.output(LedPin, GPIO.LOW)  # Set LedPin to low(0V)
 
-p = GPIO.PWM(LedPin, 1000)     # set Frequece to 1KHz
-p.start(0)                     # Duty Cycle = 0
-try:
+	p = GPIO.PWM(LedPin, 1000)     # set Frequece to 1KHz
+	p.start(0)                     # Duty Cycle = 0
+
+def loop():
 	while True:
 		for dc in range(0, 101, 4):   # Increase duty cycle: 0~100
 			p.ChangeDutyCycle(dc)     # Change duty cycle
@@ -20,8 +23,15 @@ try:
 			p.ChangeDutyCycle(dc)
 			time.sleep(0.05)
 		time.sleep(1)
-except KeyboardInterrupt:
+
+def destroy():
 	p.stop()
 	GPIO.output(LedPin, GPIO.HIGH)    # turn off all leds
 	GPIO.cleanup()
 
+if __name__ == '__main__':     # Program start from here
+	setup()
+	try:
+		loop()
+	except KeyboardInterrupt:  # When 'Ctrl+C' is pressed, the child program destroy() will be  executed.
+		destroy()
--- 10_74HC595_LED.py
+++ 10_74HC595_LED.py
@@ -1,4 +1,15 @@
 #!/usr/bin/env python
+#================================================
+#
+#	This program is for SunFounder SuperKit for Rpi.
+#
+#	Extend use of 8 LED with 74HC595.
+#
+#	Change the	WhichLeds and sleeptime value under
+#	loop() function to change LED mode and speed.
+#
+#=================================================
+
 import RPi.GPIO as GPIO
 import time
 
@@ -6,7 +17,17 @@ SDI   = 11
 RCLK  = 12
 SRCLK = 13
 
-WhichLeds = [0x01,0x02,0x04,0x08,0x10,0x20,0x40,0x80]
+#===============   LED Mode Defne ================
+#	You can define yourself, in binay, and convert it to Hex 
+#	8 bits a group, 0 means off, 1 means on
+#	like : 0101 0101, means LED1, 3, 5, 7 are on.(from left to right)
+#	and convert to 0x55.
+
+LED0 = [0x01,0x02,0x04,0x08,0x10,0x20,0x40,0x80]	#original mode
+LED1 = [0x01,0x03,0x07,0x0f,0x1f,0x3f,0x7f,0xff]	#blink mode 1
+LED2 = [0x01,0x05,0x15,0x55,0xb5,0xf5,0xfb,0xff]	#blink mode 2
+LED3 = [0x02,0x03,0x0b,0x0f,0x2f,0x3f,0xbf,0xff]	#blink mode 3
+#=================================================
 
 def print_msg():
 	print 'Program is running...'
@@ -34,16 +55,18 @@ def hc595_out():
 	GPIO.output(RCLK, GPIO.LOW)
 
 def loop():
+	WhichLeds = LED0	# Change Mode, modes from LED0 to LED3
+	sleeptime = 0.1		# Change speed, lower value, faster speed
 	while True:
 		for i in range(0, len(WhichLeds)):
 			hc595_in(WhichLeds[i])
 			hc595_out()
-			time.sleep(0.1)
+			time.sleep(sleeptime)
 		
 		for i in range(len(WhichLeds)-1, -1, -1):
 			hc595_in(WhichLeds[i])
 			hc595_out()
-			time.sleep(0.1)
+			time.sleep(sleeptime)
 
 def destroy():   # When program ending, the function is executed. 
 	GPIO.cleanup()
